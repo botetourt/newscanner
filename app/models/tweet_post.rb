063e9f62a34a1286c187206c11ba898393fcee85
Tindell Lockett <iam@tindell.net>
2017-11-14 21:27:55 -0500

    another try with the api

--- app/models/tweet_post.rb
+++ app/models/tweet_post.rb
@@ -2,13 +2,14 @@ class TweetPost < ActiveRecord::Base
   def initialize
     time = Time.now
 
-    client = Twitter::REST::Client.new do |config|      
+    client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["CONSUMER_KEY"]
       config.consumer_secret     = ENV["CONSUMER_SECRET"]
       config.access_token        = ENV["ACCESS_TOKEN"]
       config.access_token_secret = ENV["ACCESS_SECRET"]
     end
 
-    client.update "User viewed page at " + time.asc_time 
+    client.update "User viewed page at " + time.asctime
+    p "User viewed page at " + time.asctime
   end
 end
